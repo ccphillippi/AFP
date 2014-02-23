c7ac228f26e9115fedd6b3097fc1375249ccb534
verblee <yy.albert.lee@gmail.com>
2014-02-22 23:03:13 -0800

    Updated Scraping

--- afp/sentiment/NEWS_Scrap.py
+++ afp/sentiment/NEWS_Scrap.py
@@ -1,7 +1,45 @@
 import nltk
+from os import listdir
+
+keyword="google"
+path="/Users/Lee/Dropbox/AFPdb/Corpus/LexisNexis/"
+year=listdir(path)
+year.remove('.DS_Store')
+
+year=["2012"]
+
+for y in year:
+    month=listdir(path+y)
+    month.remove('.DS_Store')
+    for m in month:
+        day=listdir(path+y+"/"+m)
+        day.remove('.DS_Store')
+        for d in day:
+            Sent=[]
+            Newspaper=listdir(path+y+"/"+m+"/"+d)
+            #Newspaper.remove('.DS_Store')
+            for np in Newspaper:
+                News=listdir(path+y+"/"+m+"/"+d+"/"+np)
+                for file in News:
+                    f=open(path+y+"/"+m+"/"+d+"/"+np+"/"+file)
+                    TXT[0]=f.readlines()
+                    TXT=TXT.replace("\r", "");
+                    TXT=TXT.replace("|", ".");
+                    TXT=TXT.replace("\n", "");                    
+                    Sent=Sent+nltk.sent_tokenize(TXT)
+'''                    
+for i in Sent:
+    Sent_Split=i.lower().split()
+    if len(Sent_Split)>0:
+        if Sent_Split[0] in Mon:
+            date=str(dic[Sent_Split[0]])+"/"+Sent_Split[1][0:len(Sent_Split[1])-1]+"/"+Sent_Split[2]
+    if keyword.lower() in Sent_Split:
+        Key_Sent.append((date,i.lower()))
+
+        
 f=open('SP5.txt')
 TXT=f.readlines()
-'''
+
 temp=""
 art=[]
 for i in TXT:
@@ -12,7 +50,7 @@ for i in TXT:
         temp=temp + " " + i[:len(i)-2]
 print "done with combining paragraph"
 TXT=art
-'''
+
 date=""
 Sent=[]
 Mon=["january","february","march","april","may","june","july","august","september","october","november","december"]
@@ -47,3 +85,4 @@ for i in Key_Sent:
 #print txt
 
 #S=nltk.tokenize(txt)
+'''
